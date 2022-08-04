a256aaa066c516e9a9183f243864ffc24317552b
Andrei Kagu <andreikagu03@gmail.com>
2022-08-04 11:43:33 -0700

    [Feature-1] Solving first task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("22");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// - Составить программу обмена значениями двух переменных (при возможности не создавать третью переменную).
+
+Console.WriteLine("Добро пожаловать акуна макака! Введи пожалуйста 2 числа.");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("<==================================>");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+// a = 5
+// b = 10
+
+int num = (int)a;
+
+a = b;
+b = (int)num;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
