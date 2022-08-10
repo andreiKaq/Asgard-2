c77b0affeaacf82f9934f71387d841a5a49c5eb9
Andrei Kagu <andreikagu03@gmail.com>
2022-08-10 06:06:10 -0700

    [4TASKS] Have been compleated main 4 tasks

--- Program.cs
+++ Program.cs
@@ -1,42 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-
-
-// - TASK [1]
-// - Составить программу обмена значениями двух переменных (при возможности не создавать третью переменную).
+﻿// [TASK - 1]
+/*
+- Создать консольное приложение:
+    -Приложение должно создавать массив целых чисел, размер которого должен задавать пользователь с клавиатуры.
+*/
 
 
 
 /*
-Console.WriteLine("Добро пожаловать акуна макака! Введи пожалуйста 2 числа.");
+int input = Convert.ToInt32(Console.ReadLine());
 
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[input];
+*/
 
 
-Console.ForegroundColor = ConsoleColor.Green;
+// [TASK - 2]
+/*
+- Массив необходимо заполнить произвольными целыми числами (для генерации чисел нужно использовать     Random rand = new Random();
+int value = rand.Next(-100, 100);). Сгенерированный массив необходимо вывести на экран
+*/
 
-Console.WriteLine(a);
-Console.WriteLine(b);
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("<==================================>");
 
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.White;
 
-int num = (int)a;
 
-a = b;
-b = (int)num;
+/*
+for (int i = 0; i < array.Length - 1; i++)
+{
+    Random rand = new Random();
+    int value = rand.Next(-100, 100);
 
-Console.WriteLine(a);
-Console.WriteLine(b);
+    array[i] = value;
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
+*/
+
+
+
+// [TASK - 3]
+/*
+- С помощью математических операций, условий и циклов на консоль необходимо вывести наибольшее значение в массиве,
+ наименьшее значение в массиве, общую сумму всех элементов, среднее арифметическое всех элементов.
+ Вычисленные значения необходимо вывести на экран
+- Программа должна ожидать нажатия клавиши клавиатуры для завершения работы программы.
+*/
+
+
+/*
+int biggestNum = 0;
+int smallestNum = 0;
+int arraySum = 0;
+float arrayAvg = 0;
+
+for (int i = 0; i < array.Length - 1; i++)
+{
+    if (biggestNum < array[i]) biggestNum = array[i];               // Получаем самое большое число
+
+    if (smallestNum > array[i]) smallestNum = array[i];             // Получаем самое мальенькое число
+
+    arraySum += array[i];                                           // получаем сумму всех элементов в массиве.
+
+    arrayAvg = (float)arraySum / (float)input;                      // Получаем арифметическое среднее массива 
+}
+
+Console.WriteLine($"Наибольшое число: {biggestNum}  Наименьшее число: {smallestNum}  Сумма массива: {arraySum}  арифметическое средняя: {arrayAvg}$");
+Console.ReadLine();
+*/
+
+
+
+//[TASK - 4]
+/*
+- Создать программу-переводчик, которая знает 10 английских слов.
+Пользователь должен ввести слово на английском, программа выводит перевод на русском.
+В случае если слова нет, то нужно вывести на консоль сообщение "Слово не было найдено".
+Для решения задачи можно использовать класс Dictionary<string, string> и методы этого класса, такие как ContainsKey()....
+*/
+
+
+Dictionary<string, string> vocabulary = new Dictionary<string, string>(){
+    {"Car", "Машина"},
+    {"Person", "Человек"},
+    {"Love", "Любовь"},
+    {"Future", "Будущие"},
+    {"Might", "Мощь"},
+    {"Keyboard", "Клавиатура"},
+    {"Beer", "Пиво"},
+    {"Vodka", "Водка"},
+    {"Whisky", "Виски"},
+    {"Dog", "Собака"},
+
+    {"car", "Машина"},
+    {"person", "Человек"},
+    {"love", "Любовь"},
+    {"future", "Будущие"},
+    {"might", "Мощь"},
+    {"keyboard", "Клавиатура"},
+    {"beer", "Пиво"},
+    {"vodka", "Водка"},
+    {"whisky", "Виски"},
+    {"dog", "Собака"}
+};
+
+while (true)
+{
+    string word = Console.ReadLine();
+    if (vocabulary.ContainsKey(word))
+    {
+        Console.WriteLine(vocabulary[word]);
+    }
+    else Console.WriteLine("Слово не было найдено");
+
+}
+
+/*
+Console.WriteLine(vocabulary);
+Console.ReadLine();
 */
 
 
@@ -46,85 +124,103 @@ Console.WriteLine(b);
 
 
 
-// - TASK [2]
-// Реализовать сравнение двух введенных с клавиатуры чисел. После сравнения, программа не должна закрываться, а должна ожидать ввод следующих цифр для сравнения
-
-/*
-Console.WriteLine("Ну привет! Введи 2 числа");
-
-while (true)
-{
-    int first = Convert.ToInt32(Console.ReadLine());
-    int second = Convert.ToInt32(Console.ReadLine());
-
-    if (first is int && second is int)
-    {
-        if (first == second)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Awesome! That 2 numbers are eaqual!");
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Oh shit! That 2 numbers are not eaqual! Try again ok?");
-
-        }
-    }
-}
-*/
 
 
 
 
 
-// - TASK [3]*
-//Реализовать алгоритм, который определяет является ли введенное целочисленное число полиндромом (читается одинаково слева направо и справа налево, для преобразования к типу int использовать Convert.ToInt32())
-// ==> 123321
-// ==> ture
-// ==> 134234
-// ==> false
 
 
 
 
 
-while (true)
-{
-    Console.WriteLine("Салам Алейкум брат. Введи число и мы проверим за тебя является ли оно полиндромом xD ");
-
-    string value = Console.ReadLine();
-
-
-    int length = value.Length;
-
-
-    string first = value.Substring(0, length / 2);
-    string second = value.Substring(length / 2);
 
 
 
-    string originalString = second;
-    string reverseSecond = string.Empty;
-    for (int i = originalString.Length - 1; i >= 0; i--)
-    {
-        reverseSecond += originalString[i];
-    }
 
 
-    if (Convert.ToInt32(first) == Convert.ToInt32(reverseSecond))
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Ваше число полиндромом");
-        Console.ForegroundColor = ConsoleColor.White;
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Ваше число не полиндром");
-        Console.ForegroundColor = ConsoleColor.White;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
